ee55e425ecdbd3efa69a1e9b3b470e1e5401dffd
brandonkrakowsky <lbrandon@wharton.upenn.edu>
2019-12-02 17:25:01 -0500

    Added printer class for printing messages.

--- HellowWorldGit/src/HellowWorld.java
+++ HellowWorldGit/src/HellowWorld.java
@@ -2,13 +2,13 @@
 public class HellowWorld {
 
 	public static void main(String[] args) {
-		System.out.println(
+		MyPrinter.print(
 				"Welcome to Hello world project in git!");
-		System.out.println("Tell me your name: ");
+		MyPrinter.print("Tell me your name: ");
 		
 		MyScanner myScanner = new MyScanner();
 		String input = myScanner.getInput();
-		System.out.println("Hello " + input + "!");
+		MyPrinter.print("Hello " + input + "!");
 
 	}
 
---
+++ HellowWorldGit/src/MyPrinter.java
@@ -0,0 +1,15 @@
+/**
+ * Manages printing.
+ * @author lbrandon
+ *
+ */
+public class MyPrinter {
+
+	/**
+	 * Prints the given message on its own line.
+	 * @param message to print
+	 */
+	public static void print(String message) {
+		System.out.println(message);
+	}
+}
