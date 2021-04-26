1d408b6a99f08f00a8af4e057939c724f16c22fe
Matthew Lungren <mlungren@stanford.edu>
2021-04-25 21:41:10 -0700

    Update HellowWorld.java

--- HellowWorldGit/src/HellowWorld.java
+++ HellowWorldGit/src/HellowWorld.java
@@ -3,7 +3,7 @@ public class HellowWorld {
 
 	public static void main(String[] args) {
 		MyPrinter.print(
-				"Welcome to Hello world project in git!");
+				"Welcome to Hello world project in git! I am an awesome professor for 591 :)");
 		MyPrinter.print("Tell me your name: ");
 		
 		MyScanner myScanner = new MyScanner();
